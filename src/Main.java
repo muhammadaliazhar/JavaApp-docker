32b5405f73d1560a16acf4c3724d00527158fb7c
ali.azhar <aliazhat123@gmail.com>
2025-01-01 10:44:55 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello ALi, Java App is successfully deployed on container! Current date: " + currentDate);
+    }
+}
